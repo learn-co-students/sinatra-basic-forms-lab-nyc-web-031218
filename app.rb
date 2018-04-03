2a00977e6ffa5d7b1d436f1da96b77a30cf4dcd8
Rani Shah <rani.shah@flatironschool.com>
2018-04-03 19:00:40 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,22 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/puppy' do
+    @name = params[:puppy_name]
+    @breed = params[:puppy_breed]
+    @age = params[:puppy_age]
+    erb :display_puppy
+  end
+
 
 end
