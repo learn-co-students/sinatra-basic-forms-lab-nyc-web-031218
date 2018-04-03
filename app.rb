815b6d33c5105f09d37aee82e9d08785257ae47d
harim1206 <harim1206@gmail.com>
2018-04-03 16:28:39 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,24 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+    get '/' do
+      erb :index
+    end
+
+    get '/new' do
+      erb :create_puppy
+    end
+
+    post '/new' do
+      # binding.pry
+      # hash = {
+      #   name: params[:name],
+      #   breed: params[:breed],
+      #   age: params[:age]
+      #
+      # }
+      @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+      # binding.pry
+      erb :display_puppy
+    end
 end
